d9166eb69d5213b3f549c5a7ade8edfff596c895
Naresh Bhosale <91016022+WebNaresh@users.noreply.github.com>
2026-06-06 10:59:44 +0530

    fix(phone-input): update phone number input to use custom input component

--- apps/web/components/ui/phone-input.tsx
+++ apps/web/components/ui/phone-input.tsx
@@ -1,9 +1,8 @@
 "use client";
 
 import * as React from "react";
-import PhoneInputPrimitive, {
-  type Country,
-} from "react-phone-number-input";
+import PhoneInputPrimitive from "react-phone-number-input";
+import type { Country } from "react-phone-number-input";
 import "react-phone-number-input/style.css";
 import { cn } from "@/lib/utils";
 
@@ -13,34 +12,40 @@ export interface PhoneInputProps
     "onChange" | "value"
   > {
   value?: string;
-  onChange?: (value: string | undefined) => void;
+  onChange?: (value?: string) => void;
   defaultCountry?: Country;
 }
 
+const CustomInput = React.forwardRef<
+  HTMLInputElement,
+  React.InputHTMLAttributes<HTMLInputElement>
+>((props, ref) => (
+  <input
+    ref={ref}
+    {...props}
+    className="bg-transparent outline-none flex-1 min-w-0 text-sm placeholder:text-muted-foreground/50"
+  />
+));
+CustomInput.displayName = "CustomInput";
+
 const PhoneInput = React.forwardRef<HTMLInputElement, PhoneInputProps>(
-  ({ className, value, onChange, defaultCountry = "IN", ...props }, ref) => {
+  ({ className, value, onChange, defaultCountry = "IN", ...props }, _ref) => {
     return (
       <PhoneInputPrimitive
         international
         defaultCountry={defaultCountry}
         value={value}
-        onChange={onChange}
+        onChange={onChange ?? (() => {})}
         className={cn(
           "flex h-11 w-full rounded-md border border-input bg-background px-3 py-2 text-sm",
-          "focus-within:ring-1 focus-within:ring-primary/20 focus-within:border-primary",
+          "focus-within:ring-1 focus-within:ring-primary/10 focus-within:border-primary/40",
           "[&_.PhoneInputInput]:bg-transparent [&_.PhoneInputInput]:outline-none [&_.PhoneInputInput]:flex-1 [&_.PhoneInputInput]:min-w-0",
           "[&_.PhoneInputCountrySelect]:bg-background [&_.PhoneInputCountrySelect]:border-0 [&_.PhoneInputCountrySelect]:outline-none [&_.PhoneInputCountrySelect]:text-sm [&_.PhoneInputCountrySelect]:mr-2",
           "[&_.PhoneInputCountryIcon]:w-6 [&_.PhoneInputCountryIcon]:h-4 [&_.PhoneInputCountryIcon]:mr-1",
           className
         )}
-        inputComponent={React.forwardRef<HTMLInputElement>((inputProps, inputRef) => (
-          <input
-            ref={inputRef ?? ref}
-            {...inputProps}
-            className="bg-transparent outline-none flex-1 min-w-0 text-sm placeholder:text-muted-foreground/50"
-          />
-        ))}
-        {...props}
+        inputComponent={CustomInput}
+        {...(props as object)}
       />
     );
   }
