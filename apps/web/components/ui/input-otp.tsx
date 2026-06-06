db44bcb2f924dcb68dfe8958e7979d5f9a0607e3
Naresh Bhosale <91016022+WebNaresh@users.noreply.github.com>
2026-06-06 10:59:43 +0530

    fix(ui/input-otp.tsx): update index watching logic

--- apps/web/components/ui/input-otp.tsx
+++ apps/web/components/ui/input-otp.tsx
@@ -52,7 +52,7 @@ function InputOTPSlot({
         "relative flex h-9 w-9 items-center justify-center",
         "border-y border-r border-input text-sm shadow-xs transition-all",
         "first:rounded-l-md first:border-l last:rounded-r-md",
-        "data-[active=true]:z-10 data-[active=true]:ring-1 data-[active=true]:ring-ring data-[active=true]:border-ring",
+        "data-[active=true]:z-10 data-[active=true]:ring-1 data-[active=true]:ring-ring/30 data-[active=true]:border-ring/40",
         "data-[has-caret=true]:after:animate-caret-blink data-[has-caret=true]:after:absolute data-[has-caret=true]:after:inset-0 data-[has-caret=true]:after:flex data-[has-caret=true]:after:items-center data-[has-caret=true]:after:justify-center data-[has-caret=true]:after:font-thin data-[has-caret=true]:after:content-['|']",
         className
       )}
