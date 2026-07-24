bd0f0237e5094e62c32cd33f06fc0f955a930fb7
Naresh Bhosale <91016022+WebNaresh@users.noreply.github.com>
2026-07-24 15:39:12 +0530

    feat(report-ui): add package entry point/barrel export (#297)

---
+++ packages/report-ui/src/index.ts
@@ -0,0 +1,17 @@
+"use client";
+
+export { ReportDialog } from "./report-dialog";
+export { AnnotationCanvas } from "./annotation-canvas";
+export {
+  GLITCHGRAB_SHORTCUT,
+  GLITCHGRAB_SHORTCUT_MAC,
+  getShortcutLabel,
+  matchesShortcut,
+} from "./shortcut";
+export type {
+  ReportType,
+  ReportSeverity,
+  ReportResult,
+  ReportFn,
+  EnhanceTextFn,
+} from "./types";
