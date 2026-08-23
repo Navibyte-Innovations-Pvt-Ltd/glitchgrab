e05d845198876f030ebd096b2ef1c13c553ea1b0
Naresh Bhosale <91016022+WebNaresh@users.noreply.github.com>
2026-08-23 15:35:34 +0530

    refactor(report-ui): add assist sheet component to index.ts

--- packages/report-ui/src/index.ts
+++ packages/report-ui/src/index.ts
@@ -2,6 +2,7 @@
 
 export { ReportDialog } from "./report-dialog";
 export { AnnotationCanvas } from "./annotation-canvas";
+export { AssistSheet } from "./assist-sheet";
 export { ATTACHMENT_ACCEPT } from "./attachments";
 export {
   encodeScreenshot,
@@ -21,5 +22,8 @@ export type {
   ReportFn,
   FeedbackFn,
   EnhanceTextFn,
+  AssistFn,
+  AssistTurnParams,
+  AssistTurnResult,
   ReportReporter,
 } from "./types";
