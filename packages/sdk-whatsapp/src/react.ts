32df1cc918a92246537a380a1b3899936f1223c9
Naresh Bhosale <91016022+WebNaresh@users.noreply.github.com>
2026-09-02 10:57:31 +0530

    feat: add react support

---
+++ packages/sdk-whatsapp/src/react.ts
@@ -0,0 +1,5 @@
+"use client";
+
+export { WhatsappInbox, type WhatsappInboxProps } from "./inbox";
+export { useInbox, type UseInboxOptions, type UseInboxResult } from "./use-inbox";
+export type { WaConversation, WaMessage, WaAgent } from "./types";
