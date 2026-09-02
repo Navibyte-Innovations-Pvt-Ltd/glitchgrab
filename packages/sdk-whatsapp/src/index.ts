b26dba43ed20c6cc25c80f233711c2883cc1e596
Naresh Bhosale <91016022+WebNaresh@users.noreply.github.com>
2026-09-02 10:57:30 +0530

    fix(sw): add index exports

---
+++ packages/sdk-whatsapp/src/index.ts
@@ -0,0 +1,4 @@
+export { WhatsappClient, createWhatsappClient, type WhatsappClientOptions } from "./client";
+export { createInboxHandler, type InboxHandlerOptions } from "./handler";
+export { launchSignup, type SignupOutcome } from "./browser";
+export * from "./types";
